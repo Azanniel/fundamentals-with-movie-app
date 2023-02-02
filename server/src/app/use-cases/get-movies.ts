0f30f5b8cb5ce2b200d18a5f61488f9edbb36c8d
azanniel <leo.azannielttt@gmail.com>
2023-02-02 13:05:33 -0400

    ♻️ refactor get movies use case

--- server/src/app/use-cases/get-movies.ts
+++ server/src/app/use-cases/get-movies.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { Movie } from '../entities/movie';
 import { MoviesRepository } from '../repositories/movies-repository';
+
+interface GetMoviesResponse {
+  movies: Movie[];
+}
 
 @Injectable()
 export class GetMovies {
   constructor(private moviesRepository: MoviesRepository) {}
 
-  async execute() {
+  async execute(): Promise<GetMoviesResponse> {
     const movies = await this.moviesRepository.findMany();
 
     return { movies };
